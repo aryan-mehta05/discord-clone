187b4cb00ec2a51d1933090933d9ad2bec01e067
Aryan Mehta <aryannmehta@gmail.com>
2023-09-13 22:15:41 +0530

    Add: Frontend requests for coversations and members -- search bar

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { Search } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
 
 import { 
   CommandDialog, 
@@ -28,6 +29,8 @@ export const ServerSearch = ({
   data
 }: ServerSearchProps) => {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const params = useParams();
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
@@ -40,6 +43,18 @@ export const ServerSearch = ({
     document.addEventListener("keydown", down);
     return () => document.removeEventListener("keydown", down);
   }, []);
+
+  const onClick = ({ id, type }: { id: string, type: "channel" | "member" }) => {
+    setOpen(false);
+
+    if (type === "member") {
+      return router.push(`/servers/${params?.serverId}/conversations/${id}`);
+    }
+
+    if (type === "channel") {
+      return router.push(`/servers/${params?.serverId}/channels/${id}`);
+    }
+  }
 
   return (
     <>
@@ -72,7 +87,7 @@ export const ServerSearch = ({
               <CommandGroup key={label} heading={label}>
                 {data?.map(({ id, icon, name}) => {
                   return (
-                    <CommandItem key={id}>
+                    <CommandItem key={id} onSelect={() => onClick({ id, type })}>
                       {icon}
                       <span>{name}</span>
                     </CommandItem>
