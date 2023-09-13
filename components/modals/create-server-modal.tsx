b73b3b86960b03e610386f0c6988cf9a61f3063d
Aryan Mehta <aryannmehta@gmail.com>
2023-09-13 11:07:44 +0530

    Add: Invite modal and generate new link functionality; Update: Fix CSS error;

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -72,7 +72,7 @@ export const CreateServerModal = () => {
   
   return (
     <Dialog open={isModalOpen} onOpenChange={handleClose}>
-      <DialogContent className="bg-white text-black- p-0 overflow-hidden">
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
             Customize your server
@@ -135,4 +135,4 @@ export const CreateServerModal = () => {
       </DialogContent>
     </Dialog>
   )
-}
+};
