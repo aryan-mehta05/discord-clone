84c864a73e5bdffdaa81cd2c81e7f480a7113e1a
Aryan Mehta <aryannmehta@gmail.com>
2023-09-13 17:47:50 +0530

    Update: Change delete server button CSS

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -3,6 +3,33 @@ import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/current-profile";
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: {
+    serverId: string
+  } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
+
 export async function PATCH(
   req: Request,
   { params }: { params: {
@@ -33,4 +60,4 @@ export async function PATCH(
     console.log("[SERVER_ID_PATCH]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
-}
+};
