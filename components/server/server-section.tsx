67f346d0c7454d271047cfdccf59f2a142ba3338
Aryan Mehta <aryannmehta@gmail.com>
2023-09-13 23:35:09 +0530

    Add: Plus button on server-sidebar preselects channel-type

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -28,8 +28,8 @@ export const ServerSection = ({
       {role !== MemberRole.GUEST && sectionType === "channels" && (
         <ActionTooltip label="Create Channel" side="top">
           <button
-          onClick={() => onOpen("createChannel")}
-            className="text-zinc-50 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+          onClick={() => onOpen("createChannel", { channelType })}
+            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
           >
             <Plus className="h-4 w-4" />
           </button>
@@ -39,7 +39,7 @@ export const ServerSection = ({
         <ActionTooltip label="Manage Members" side="top">
           <button
           onClick={() => onOpen("members", { server })}
-            className="text-zinc-50 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+            className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
           >
             <Settings className="h-4 w-4" />
           </button>
